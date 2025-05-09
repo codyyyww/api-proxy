f22c023fae426f538429ca78095ad217f56fec11
codyyyww <ookk1520@gmail.com>
2025-05-08 23:01:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors()); 
 
 app.post('/api/chat', async (req, res) => {
   try {
